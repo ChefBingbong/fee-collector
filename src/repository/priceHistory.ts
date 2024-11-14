61ee6a62ffbf7ff40dcf9a96bbe638d8e7c3d0bd
Evan McGrane <evanmcgrane@Evans-MacBook-Pro.local>
2024-11-14 23:23:56 +0000

    add mongo db actions

--- src/repository/priceHistory.ts
+++ src/repository/priceHistory.ts
@@ -1,1 +1,74 @@
-export class PriceHistoryRepository {}
+import { Connection, Model } from "mongoose";
+import { IPriceData, getPriceHistorySchema } from "../db/schemas/token-price.schema";
+import { ModelType } from "../db/types";
+import { AppLogger } from "../logging/logger";
+import { chunks } from "../utils/dbUtils";
+
+export class PriceHistoryRepository extends AppLogger {
+	public readonly collectionId: string;
+	private readonly connection: Connection;
+	private readonly model: Model<IPriceData>;
+
+	constructor(id: string, connection: Connection) {
+		super(`${id}-logger`);
+		this.collectionId = id;
+		this.connection = connection;
+		this.model = connection.model<IPriceData>(ModelType.priceHistoryInfo, getPriceHistorySchema());
+	}
+
+	public addMany = async (priceHistoryData: IPriceData[]) => {
+		if (priceHistoryData.length === 0) {
+			this.logger.error("[VaultHistoryRepository] [add] No vault history to add");
+			throw new Error("No vault history to add");
+		}
+
+		for (const chunk of chunks(priceHistoryData, 100)) {
+			try {
+				const result = await this.model.insertMany<IPriceData>(chunk);
+				this.logger.info(`[PriceInfoRepository] [add] Inserted ${result?.length} vault history`);
+			} catch (error) {
+				this.logger.error(`[PriceInfoRepository] [add] Error inserting price history for : [${this.collectionId}] -> ${error}`);
+			}
+		}
+	};
+
+	public addOne = async (priceHistoryData: IPriceData) => {
+		if (!priceHistoryData) {
+			this.logger.error("[VaultHistoryRepository] [addOne] No vault history to add");
+			throw new Error("No vault history to add");
+		}
+
+		try {
+			await this.model.create<IPriceData>(priceHistoryData);
+			this.logger.info(`[PriceInfoRepository] [addOne] Inserted vault history`);
+		} catch (error) {
+			this.logger.error(`[PriceInfoRepository] [addOne] Error inserting vault history for : [${this.collectionId}] -> ${error}`);
+			throw new Error(`Error inserting vault history for ${this.collectionId}`);
+		}
+	};
+
+	public get = async (timestamp: number): Promise<IPriceData | null> => {
+		try {
+			return await this.model.findOne({ timestamp: timestamp });
+		} catch (error) {
+			this.logger.error(`[PriceInfoRepository] [get] Error getting vault history for : [${this.collectionId}] - [${error}]`);
+			throw new Error(`Error getting vault history for ${this.collectionId}`);
+		}
+	};
+
+	public getByRange = async (startTimestamp: number, endTimestamp: number): Promise<IPriceData[]> => {
+		try {
+			return await this.model.find({
+				timestamp: {
+					$gte: startTimestamp,
+					$lte: endTimestamp,
+				},
+			});
+		} catch (error) {
+			this.logger.error(`[PriceInfoRepository] [get] Error getting vault history for : [${this.collectionId}] - [${error}]`);
+			throw new Error(`Error getting vault history for ${this.collectionId}`);
+		}
+	};
+
+	public getConnection = () => this.connection;
+}
---
+++ src/utils/dbUtils.ts
@@ -0,0 +1,46 @@
+export const equalsIgnoreCase = (a: string, b: string) => {
+	return a.localeCompare(b, undefined, { sensitivity: "accent" }) === 0;
+};
+
+export const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const hasRequiredFields = (field: any[]): boolean => {
+	return field.filter((f) => !isBlank(f)).length === 0;
+};
+
+export const isBlank = (f: string): boolean => {
+	return f === undefined || f === null || f === "" || f === "" || f === " ";
+};
+
+export const isEmpty = (f: {}): boolean => {
+	return Object.keys(f).length === 0 || f === undefined || f === null;
+};
+
+export const hasRequiredFieldsInObject = (object: {}): boolean => {
+	for (const key of Object.keys(object)) {
+		if (isBlank(object[key])) {
+			console.error(`[hasRequiredFieldsInObject] Missing required field: ${key}, value: ${object[key]}`);
+			return false;
+		}
+	}
+	return true;
+};
+
+export const chunks = <T>(array: Array<T>, chunkSize: number): Array<T>[] => {
+	const chunks: Array<T>[] = [];
+	for (let i = 0; i < array.length; i += chunkSize) {
+		chunks.push(array.slice(i, i + chunkSize));
+	}
+	return chunks;
+};
+
+export const getFlooredTimeStamp = (timestamp: number, nthDayFromToday: number): number => {
+	if (!timestamp) {
+		console.error(`[getFlooredTimeStamp] Invalid timestamp: ${timestamp}`);
+		throw new Error(`Invalid timestamp: ${timestamp}`);
+	}
+	const date = new Date(timestamp);
+	date.setDate(date.getDate() - nthDayFromToday);
+	date.setHours(0, 0, 0, 0);
+	return date.getTime() / 1000;
+};
